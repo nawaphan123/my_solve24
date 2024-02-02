03db706d94e4980ba57480122f3a174747793ceb
Nawa phansaen <79089602+nawaphan123@users.noreply.github.com>
2024-02-02 14:38:26 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -2,16 +2,41 @@ const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
 
+const judgePoint24 = function (nums,point) {
+    const isValid = x => Math.abs(x - point) < 0.0000001
+    const aux = (arr = []) => {
+      if (arr.length === 1) {
+        return isValid(arr[0])
+      }
+      let valid = false
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+          const nextArr = arr.filter((x, index) => index !== i && index !== j)
+          valid = valid || aux([...nextArr, arr[i] + arr[j]])
+            || aux([...nextArr, arr[i] - arr[j]])
+            || aux([...nextArr, arr[j] - arr[i]])
+            || aux([...nextArr, arr[i] * arr[j]])
+            || aux([...nextArr, arr[i] / arr[j]])
+            || aux([...nextArr, arr[j] / arr[i]])
+        }
+      }
+      return valid
+    }
+  
+    return aux(nums)
+  }
+
+
 class Scratch3YourExtension {
 
-    constructor (runtime) {
+    constructor(runtime) {
         // put any setup for your extension here
     }
 
     /**
      * Returns the metadata about your extension.
      */
-    getInfo () {
+    getInfo() {
         return {
             // unique ID for your extension
             id: 'yourScratchExtension',
@@ -50,7 +75,7 @@ class Scratch3YourExtension {
                     //   TargetType.SPRITE - for code in sprites
                     //   TargetType.STAGE  - for code on the stage / backdrop
                     // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+                    filter: [TargetType.SPRITE, TargetType.STAGE],
 
                     // arguments used in the block
                     arguments: {
@@ -81,6 +106,59 @@ class Scratch3YourExtension {
                             type: ArgumentType.STRING
                         }
                     }
+                },
+                {
+                    // name of the function where your block code lives
+                    opcode: 'myFirstBlock2',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'check [MY_STRING2] can calculate to [MY_NUMBER2]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [TargetType.SPRITE, TargetType.STAGE],
+
+                    // arguments used in the block
+                    arguments: {
+                        MY_NUMBER2: {
+                            // default value before the user sets something
+                            defaultValue: 24,
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.NUMBER
+                        },
+                        MY_STRING2: {
+                            // default value before the user sets something
+                            defaultValue: '1234',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
                 }
             ]
         };
@@ -91,9 +169,21 @@ class Scratch3YourExtension {
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    myFirstBlock ({ MY_STRING }) {
+    myFirstBlock({ MY_STRING }) {
         // example implementation to return a string
         return eval(MY_STRING);
+    }
+    myFirstBlock2({MY_STRING2,MY_NUMBER2})
+    {
+        let a = MY_STRING2.split('');
+
+        if (judgePoint24(a,MY_NUMBER2))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
 
